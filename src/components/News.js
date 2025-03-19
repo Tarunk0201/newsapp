2388c40978b464426f4e84945fc7283085b615e0
Tarunk0201 <tarun.singh.7801@gmail.com>
2025-03-19 23:39:37 +0530

    constructor added

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ export class News extends Component {
         <h2>NewsTak - Top Headlines</h2>
         <div className="row">
             <div className="col-md-4">
-              <NewsItem title="my title" description="you know" />
+              <NewsItem title="my title" description="you know" imgUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" />
             </div>
             <div className="col-md-4">
               <NewsItem title="my title" description="you know" />
